0b029463f5aa1104c8303976bf328a62d10b522b
JohnathanCh <johnchriest@gmail.com>
2023-01-06 17:18:09 -0800

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import ipdb
 
 def return_evens(num_list):
-    pass
+    # ipdb.set_trace()
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # ipdb.set_trace()
+    return [f"{sentence}!" for sentence in sentence_list]
